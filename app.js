c2a5b3812798366602a79cd3e046ebbcc78b149f
haibin <sunhaibin@goyoo.com>
2016-11-03 17:46:04 +0800

    add the router

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 'use strict';
 
 // 标准库依赖
-var fs = require('fs'),
-    path = require('path');
+var fs      = require('fs'),
+    path    = require('path');
 // 第三方库依赖
 var express = require('express'),
-    logger = require('morgan');
+    logger  = require('morgan'),
+    hbs     = require('hbs');
 // 自身依赖
-var router = require('./router'),
-	config = require('./config');
+var router  = require('./router'),
+	config  = require('./config');
 
-// 全局变量
-var app = express();
+var app     = express();
 
 // log
 app.use(logger('dev'));
-// 静态资源
+
+// static file
 app.use(express.static(path.join(__dirname, 'public'), {maxAge: 86400000}));
 
+// vies engine
+app.set('view engine', 'hbs');
+
+// rouer
 router(app);
 
-var server = app.listen(config.express.port, function () {
-  var port = server.address().port;
-
+var port = config.express.port;
+var server = app.listen(port, function () {
   console.log('Listening at %s', port);
   console.log('Env: ' + app.get('env'));
 });
--- router/index.js
+++ router/index.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var errorHandler = require('./error_handler.js');
+var pageRouter = require('../lib/page'),
+    apiRouter  = require('../lib/api');
 
 module.exports = function (app) {
 
     //ping
     app.get('/ping', function (req, res) { res.send('OK'); });
+
+    //router
+    app.use('/api', apiRouter);
+    app.use('/', pageRouter);
 
     // 404 handler
     app.use(errorHandler.handler404);
