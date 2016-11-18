2ceabf034719066266eb48467f211d44a74ee336
haibin <sunhaibin@goyoo.com>
2016-11-18 19:35:37 +0800

    use vue&webpack

---
+++ public/app/page/encryption.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+      <div class="container wrapper card-shadow">
+          <br>
+          <textarea class="form-control" draggable="false" autofocus placeholder="input"></textarea>
+          <br>
+        <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#">MD5</a></li>
+        <li role="presentation"><a href="#">SHA1</a></li>
+        <button type="button" class="btn btn-success pull-right">加密</button>
+      </ul>
+      <br>
+      <table class="table table-striped">
+          <tr>
+            <th class="col-md-2"></th>
+            <th class="col-md-8">123</th>
+          </tr>
+        <tr>
+          <td class="col-md-2">32大</td>
+          <td class="col-md-8">202cb962ac59075b964b07152d234b70</td>
+        </tr>
+        <tr>
+          <td class="col-md-2">32小</td>
+          <td class="col-md-8">202cb962ac59075b964b07152d234b70</td>
+        </tr>
+        <tr>
+          <td class="col-md-2">16大</td>
+          <td class="col-md-8">ac59075b964b0715</td>
+        </tr>
+        <tr>
+          <td class="col-md-2">16小</td>
+          <td class="col-md-8">ac59075b964b0715</td>
+        </tr>
+      </table>
+      <br><br><br><br><br><br><br><br>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ public/js/encryption.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from '../app/page/encryption.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
